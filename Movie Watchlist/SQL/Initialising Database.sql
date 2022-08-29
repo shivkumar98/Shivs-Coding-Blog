4c38dbe14ac0c42afd4451d13349ca73f5a48620
shivkumar98 <58525809+shivkumar98@users.noreply.github.com>
2022-08-29 10:33:51 +0100

    Create Initialising Database.sql

---
+++ Movie Watchlist/SQL/Initialising Database.sql
@@ -0,0 +1,15 @@
+use moviedb;
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM directors WHERE 1=1;
+DELETE FROM movies WHERE 1=1;
+DELETE FROM reviews WHERE 1=1;
+SELECT * FROM movies;
+SELECT * FROM directors;
+
+
+INSERT INTO movies( director_id, movie_title, movie_watched, review_id, date_watched)
+VALUES ( null, 'Prisoners', true, null, '2013/01/01');
+SELECT movie_id AS LastID FROM movies WHERE movie_id = @@Identity;
+INSERT INTO directors ( movie_id,director_name) VALUES
+((select movie_id FROM movies WHERE movie_id=@@Identity), 'Denis Villeneuve');
+
